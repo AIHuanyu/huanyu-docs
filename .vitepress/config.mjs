43de50bc393ccdb144bc0d21d8017c7a1420532f
AIHuanyu <aihuanyu.space@outlook.com>
2025-10-18 00:40:03 +0800

    v1.0.2 add Markdown Custom Containers

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -6,6 +6,15 @@ export default defineConfig({
   description: "寰宇无界，AI 无限 —— AI寰宇空间，分享个人技术与思考",
   head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   lastUpdated: true,
+  markdown: {
+    container: {
+      tipLabel: "提示",
+      warningLabel: "警告",
+      dangerLabel: "危险",
+      infoLabel: "信息",
+      detailsLabel: "详细信息",
+    },
+  },
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },
