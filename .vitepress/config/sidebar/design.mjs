e04e6a239e63e88be46e193e87e9357c1c02e343
AIHuanyu <aihuanyu.space@outlook.com>
2025-10-25 00:00:25 +0800

    v1.0.12 add sidebar of design

---
+++ .vitepress/config/sidebar/design.mjs
@@ -0,0 +1,12 @@
+const UrlDesign = "/docs/design/";
+export default [
+  {
+    text: "软件设计",
+    items: [
+      {
+        text: "数据模型设计",
+        link: UrlDesign + "data-model",
+      },
+    ],
+  },
+];
